479350e58785ca2a395702fd7abb69f1a4bc3a86
jsh3418 <albert5428@gmail.com>
2022-09-15 23:05:40 +0900

    fix: localStorage에 데이터가 없을 때 질문들이 사라지는 버그 수정

--- script.js
+++ script.js
@@ -67,7 +67,9 @@ const render = (element, currentPage) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 
 const ul = document.querySelector("ul.discussions__container");
-getLocalStorage("discussions");
+if (isExistLocalStorage("discussions")) {
+  getLocalStorage("discussions");
+}
 render(ul, currentPage);
 
 const submitButton = document.querySelector("#submit-button");
@@ -204,6 +206,9 @@ function setLocalStorage(data) {
 
 function getLocalStorage(keyName) {
   const discussions = JSON.parse(localStorage.getItem(keyName));
-  console.log(discussions);
   agoraStatesDiscussions = discussions;
+}
+
+function isExistLocalStorage(keyName) {
+  return Boolean(localStorage.getItem(keyName));
 }
